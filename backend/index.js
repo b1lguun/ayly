7679304cc0c14fdd5b58a3114b1cb47978a58fa5
b1lguun <bilguunbek369@gmail.com>
2023-08-09 07:46:29 +0800

    done

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,25 @@
-const express = require("express")
-const app = express()
-const port = 3333
-const fs = require("fs")
-const XLSX = require("xlsx")
-app.use(express.json())
+const express = require("express");
+const app = express();
+const port = 3333;
+const fs = require("fs");
+const XLSX = require("xlsx");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 app.get("/:type", (req, res) => {
-  const { type } = req.params
-  const file = `./data/${type}.xlsx`
-  const file1 = fs.readFileSync(file)
-  const bufferArray = file1.buffer
+  const { type } = req.params;
+  const file = `./data/${type}.xlsx`;
+  const file1 = fs.readFileSync(file);
+  const bufferArray = file1.buffer;
   const wb = XLSX.read(bufferArray, {
     type: "buffer",
-  })
-  const wsname = wb.SheetNames[0]
-  const ws = wb.Sheets[wsname]
-  const parsedData = XLSX.utils.sheet_to_json(ws)
-  res.send(parsedData)
-})
+  });
+  const wsname = wb.SheetNames[0];
+  const ws = wb.Sheets[wsname];
+  const parsedData = XLSX.utils.sheet_to_json(ws);
+  res.send(parsedData);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
